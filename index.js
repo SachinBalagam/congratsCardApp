9afa03a57460756587fd5744c6a5919d38415920
SachinBalagam <balagamsachin337@gmail.com>
2023-07-24 14:29:23 +0000

    Final Result Commit

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const element = (
-    // Write your code here.
-  )
-  
-ReactDOM.render(element, document.getElementById('root'))
+  <div className="bg-container">
+    <h1 className="title">Congratulations</h1>
+    <div className="card">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        alt="person"
+      />
+      <h1 className="name">Kiran V</h1>
+      <p className="collegeName">
+        Vishnu Institute of Computer Education and Technology, Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        alt="watch"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
